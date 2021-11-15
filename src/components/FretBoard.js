832d6afa93d339edf57d78645a9c6083f7dd07e1
Goran Antic <goran.antic@evermedtv.com>
2021-11-15 09:16:01 +0100

    Organized styles

--- src/components/FretBoard.js
+++ src/components/FretBoard.js
@@ -7,6 +7,7 @@ import {
 } from "../ducks/data";
 import { getNoteByStringAndFret } from "../utils/getNoteByStringAndFret";
 import { strings } from "../utils/stringsAndNotes";
+import styles from "./FretBoard.module.css";
 
 export default function FretBoard() {
   const buffer = useSelector(selectBuffer);
@@ -25,36 +26,33 @@ export default function FretBoard() {
   }
 
   return (
-    <table style={{ userSelect: "none", textAlign: "center" }}>
+    <table className={styles.FretBoard}>
+      <thead>
+        <tr>
+          {Array.from(Array(25).keys()).map((n) => (
+            <th>{n}</th>
+          ))}
+          <th>Special</th>
+        </tr>
+      </thead>
       <tbody>{getFreatBoardTable(onClick)}</tbody>
     </table>
   );
 }
 
 function getFreatBoardTable(onClickHandler) {
-  const fretBoard = [
-    <tr>
-      {Array.from(Array(25).keys()).map((n) => (
-        <th>{n}</th>
-      ))}
-    </tr>,
-  ];
-
+  const fretBoard = [];
   for (let i = 0; i < 6; i++) {
     const frets = [];
     let width = 3;
 
     for (let j = 0; j <= 24; j++) {
       width *= 0.96;
-      const key = strings[i] + j;
       frets.push(
         <td
-          key={key}
+          key={strings[i] + j}
           onClick={() => onClickHandler(i, j)}
           style={{ width: j ? width + "em" : "1em" }}
-          className={
-            [3, 5, 7, 9, 12, 15, 17, 19, 21].includes(j) ? "gray-cell" : ""
-          }
         >
           {getNoteByStringAndFret(strings[i], j)}
         </td>
@@ -64,12 +62,7 @@ function getFreatBoardTable(onClickHandler) {
     fretBoard.push(
       <tr key={i}>
         {frets}
-        <td
-          style={{ width: "20px", textAlign: "center" }}
-          onClick={() => onClickHandler(i, "x")}
-        >
-          x
-        </td>
+        <td onClick={() => onClickHandler(i, "x")}>x</td>
       </tr>
     );
   }
